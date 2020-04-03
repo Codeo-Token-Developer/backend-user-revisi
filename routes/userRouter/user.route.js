56a41d231b9d6e69f136515ef6c2318d825d4296
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2020-04-03 17:53:33 +0700

    tambah get one user

--- routes/userRouter/user.route.js
+++ routes/userRouter/user.route.js
@@ -1,27 +1,38 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
 
 //Auth
-const Auth = require('../../middlewares/Auth').userAuthentication;
+const Auth = require("../../middlewares/Auth").userAuthentication;
 
-const sendEmail = require('../../middlewares/sendEmailRegister');
-const sendEmailForgotPassword = require('../../middlewares/sendEmailForgotPassword');
-const sendEmailChangePassword = require('../../middlewares/sendEmaiChangePassword');
+const sendEmail = require("../../middlewares/sendEmailRegister");
+const sendEmailForgotPassword = require("../../middlewares/sendEmailForgotPassword");
+const sendEmailChangePassword = require("../../middlewares/sendEmaiChangePassword");
 
 //Function
-let { readAll, QrUpdate,create, login, logout, QrLogout,forgotPassword, changePassword } = require('../../controllers/user/userController');
+let {
+  readAll,
+  readMe,
+  QrUpdate,
+  create,
+  login,
+  logout,
+  QrLogout,
+  forgotPassword,
+  changePassword
+} = require("../../controllers/user/userController");
 
-Router.get('/',readAll);
-Router.post('/', create, sendEmail);
-Router.post('/login', login);
+Router.get("/", readAll);
+Router.get("/account", Auth, readMe);
+Router.post("/", create, sendEmail);
+Router.post("/login", login);
 
 //forgotPassword
-Router.post('/forgotPassword', forgotPassword, sendEmailForgotPassword);
+Router.post("/forgotPassword", forgotPassword, sendEmailForgotPassword);
 
 //changePassword
-Router.post('/changePassword', Auth,changePassword, sendEmailChangePassword);
-Router.patch('/logout', Auth, logout);
-Router.patch('/2faout',Auth, QrLogout);
-Router.patch('/2fa', Auth, QrUpdate);
+Router.post("/changePassword", Auth, changePassword, sendEmailChangePassword);
+Router.patch("/logout", Auth, logout);
+Router.patch("/2faout", Auth, QrLogout);
+Router.patch("/2fa", Auth, QrUpdate);
 
 module.exports = Router;
