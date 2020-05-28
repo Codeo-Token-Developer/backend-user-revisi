2fbac32d6efddd6112436bbfc4dcbcdc021f1349
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2020-05-28 15:58:28 +0700

    add cripto bit and lite

---
+++ routes/Blockchain/litecoin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Router = express.Router();
+const { userAuthentication } = require("../../middlewares/Auth");
+const {
+  generateAddress,
+  readMe,
+  info,
+  history,
+} = require("../../controllers/API/Litecoint");
+
+Router.post("/addAddress", userAuthentication, generateAddress);
+Router.get("/Address", userAuthentication, readMe);
+Router.get("/infoAccount/:Address", userAuthentication, info);
+Router.get("/historyAccount/:Address", userAuthentication, history);
+
+// Router.get('/myLogHistory')
+
+module.exports = Router;
--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,84 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
 
 //Sub Route
-const users = require('./userRouter/user.route');
-const accounts = require('./accountRouter/account.route');
-const logHistory = require('./otherRouter/logHistory.route');
-const fromEmail = require('./emailRouter/fromEmail.route');
-const historyRouter = require('./otherRouter/history.route');
-const kycRouter = require('./otherRouter/kyc.route');
-const creditCardRouter = require('./otherRouter/creditCard.route');
-const cryptoRouter = require('./otherRouter/crypto.route');
-const transferRouter = require('./transferRouter/transfer.route');
-const bankAccountRouter = require('./otherRouter/bankAccount.route');
-const feeRouter = require('./otherRouter/fee.route');
-const injectionRouter = require('./injection');
-const sandboxRouter = require('./sandboxRouter');
-const notif = require('./otherRouter/notification');
+const users = require("./userRouter/user.route");
+const accounts = require("./accountRouter/account.route");
+const logHistory = require("./otherRouter/logHistory.route");
+const fromEmail = require("./emailRouter/fromEmail.route");
+const historyRouter = require("./otherRouter/history.route");
+const kycRouter = require("./otherRouter/kyc.route");
+const creditCardRouter = require("./otherRouter/creditCard.route");
+const cryptoRouter = require("./otherRouter/crypto.route");
+const transferRouter = require("./transferRouter/transfer.route");
+const bankAccountRouter = require("./otherRouter/bankAccount.route");
+const feeRouter = require("./otherRouter/fee.route");
+const injectionRouter = require("./injection");
+const sandboxRouter = require("./sandboxRouter");
+const notif = require("./otherRouter/notification");
+const btc = require("./Blockchain/bitcoin");
+const ltc = require("./Blockchain/litecoin");
 
-Router.use('/injection', injectionRouter);
-Router.use('/sandbox', sandboxRouter);
+Router.use("/injection", injectionRouter);
+Router.use("/sandbox", sandboxRouter);
 
 //User Router
-Router.use('/users', users);
+Router.use("/users", users);
 
 //AccountRouter
-Router.use('/accounts', accounts);
-Router.use('/logHistory', logHistory);
+Router.use("/accounts", accounts);
+Router.use("/logHistory", logHistory);
 
 //Transfer
-Router.use('/transfer', transferRouter);
+Router.use("/transfer", transferRouter);
 
 //OtherRouter
-Router.use('/crypto', cryptoRouter);
-Router.use('/credit-card', creditCardRouter);
-Router.use('/kyc', kycRouter);
-Router.use('/history', historyRouter);
-Router.use('/api', fromEmail);
-Router.use('/fee', feeRouter);
-Router.use('/bankAccount', bankAccountRouter);
-Router.use('/notif', notif);
+Router.use("/crypto", cryptoRouter);
+Router.use("/credit-card", creditCardRouter);
+Router.use("/kyc", kycRouter);
+Router.use("/history", historyRouter);
+Router.use("/api", fromEmail);
+Router.use("/fee", feeRouter);
+Router.use("/bankAccount", bankAccountRouter);
+
+Router.use("/notif", notif);
+
+// bit
+Router.use("/btc", btc);
+
+//lite
+Router.use("/ltc", ltc);
 
 //Topup
-Router.use(require('./topup/topupRouter'));
+Router.use(require("./topup/topupRouter"));
 
-//cms 
-Router.use("/cms", require('./cmsRouter/cmsRouter.js'));
+//cms
+Router.use("/cms", require("./cmsRouter/cmsRouter.js"));
 
 //LaunchPadnpm
-Router.use('/project', require('./launchpadRouter/projectRouter'));
+Router.use("/project", require("./launchpadRouter/projectRouter"));
 
 //Exchange
-Router.use(require('./exchange/Trade'));
+Router.use(require("./exchange/Trade"));
 
-const Trade = require('../models/exchange/Trade');
-const Account = require('../models/AccountSide/account.model')
+const Trade = require("../models/exchange/Trade");
+const Account = require("../models/AccountSide/account.model");
 
-Router.patch('/account-test/:userId', (req,res,next) => {
-    let user = req.params.userId;
-    Account.updateOne({user}, {balance: 500000})
-        .then(() => {
-            res.send('Hallo')
-        })
-        .catch(next);
-})
+Router.patch("/account-test/:userId", (req, res, next) => {
+  let user = req.params.userId;
+  Account.updateOne({ user }, { balance: 500000 })
+    .then(() => {
+      res.send("Hallo");
+    })
+    .catch(next);
+});
 
-Router.delete('/trade', (req,res,next) => {
-    Trade.deleteMany({})
-        .then(() => {
-            res.send('Delete')
-        })
-        .catch(next)
-})
+Router.delete("/trade", (req, res, next) => {
+  Trade.deleteMany({})
+    .then(() => {
+      res.send("Delete");
+    })
+    .catch(next);
+});
 
 module.exports = Router;
