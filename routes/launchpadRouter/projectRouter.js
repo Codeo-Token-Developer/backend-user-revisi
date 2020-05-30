4fd6eb1204fda3afc981bbe5bbf024aa1daadc2d
laskar-ksatria <dock_fest@yahoo.co.id>
2020-05-30 20:19:49 +0700

    ++

--- routes/launchpadRouter/projectRouter.js
+++ routes/launchpadRouter/projectRouter.js
@@ -8,12 +8,12 @@ Router.post('/step1', (req,res,next) => {
     next();
 },userAuthentication, ProjectController.step1);
 Router.post('/step2', userAuthentication, ProjectController.step2);
-Router.post('/step3', userAuthentication, ProjectController.step3);
-Router.post('/step4', userAuthentication, ProjectController.step4);
-Router.post('/step5', userAuthentication, ProjectController.step5);
-Router.post('/step6', userAuthentication, ProjectController.step6);
-Router.post('/step7', userAuthentication, ProjectController.step7);
-Router.post('/step8', userAuthentication, ProjectController.step8);
+Router.patch('/step3', userAuthentication, ProjectController.step3);
+Router.patch('/step4', userAuthentication, ProjectController.step4);
+Router.patch('/step5', userAuthentication, ProjectController.step5);
+Router.patch('/step6', userAuthentication, ProjectController.step6);
+Router.patch('/step7', userAuthentication, ProjectController.step7);
+Router.patch('/step8', userAuthentication, ProjectController.step8);
 Router.get('/myProjects', userAuthentication, ProjectController.readMyProject);
 Router.get('/notCompleted', userAuthentication, ProjectController.readMyNotCompleted);
 
