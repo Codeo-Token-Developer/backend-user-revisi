d60d5c1b5dea980a402576bfc37f54fc81eaf70d
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-07-03 14:03:38 +0700

    Transfer Updated!

--- controllers/transfer/codeTransferController.js
+++ controllers/transfer/codeTransferController.js
@@ -1,7 +1,7 @@
 const Account = require("../../models/AccountSide/account.model");
 const TransferCodeo = require("../../helpers/codeoTransfer");
 const TransactionHistory = require("../../models/Other/transactionHistory.model");
-const accountHistory = require('../../models/AccountSide/accountHistory.model');
+const accountHistory = require("../../models/AccountSide/accountHistory.model");
 const AdminFeeHistory = require("../../models/AdminSide/adminFeeHistory");
 const Referral = require("../../models/Other/referral.model");
 const User = require("../../models/AuthSide/user.model");
@@ -9,72 +9,89 @@ const { encryptAccount, decryptAccount } = require("../../helpers/encryptKey");
 
 class CodeoTransferController {
   static sendCodeo(req, res, next) {
-    console.log('Masuk sendCodeo ==========================================================')
+    console.log(
+      "Masuk sendCodeo =========================================================="
+    );
     let user = req.decoded.id;
     let { myValue, toAddress, adminValue, text } = req.body;
+    let fundAccount;
     Account.findOne({ user })
       .then(async function (account) {
-        req.myAccount = account
-        let key = JSON.parse(JSON.stringify(account.key))
+        req.myAccount = account;
+        fundAccount = account.ETH;
+        let key = JSON.parse(JSON.stringify(account.key));
         let newKey = await decryptAccount(key);
-        return TransferCodeo(toAddress, myValue, newKey)
+        return TransferCodeo(toAddress, myValue, newKey);
       })
       .then(function (trx) {
-        console.log("ini", trx)
-        return accountHistory.create({
-          from: fundAccount,
-          transaction_id: trx,
-          transaction_status: true,
-          value: myValue,
-          to: toAddress,
-          user: user,
-          link: `https://etherscan.io/address/${fundAccount}`,
-          ket: text
-        })
-      })
-      .then(function (history) {
-        next();
-      })
-      .catch(err => {
-        let hash = "none"
-        let desc;
-        if (err.message === 'Returned error: insufficient funds for gas * price + value') {
-          desc = 'insufficient funds for gas * price + value'
-        }
-        return accountHistory.create({
-          from: fundAccount,
-          transaction_id: hash,
-          transaction_status: false,
-          value: myValue,
-          to: toAddress,
-          user: user,
-          description: desc,
-          link: `https://etherscan.io/address/${fundAccount}`,
-          ket: text
-        })
-          .then(function (history) {
-            next(err)
+        console.log("ini", trx);
+        return accountHistory
+          .create({
+            from: fundAccount,
+            transaction_id: trx,
+            transaction_status: true,
+            value: myValue,
+            to: toAddress,
+            user: user,
+            link: `https://etherscan.io/address/${fundAccount}`,
+            ket: text,
           })
-          .catch(err => {
-            res.status(200).json({ message: 'Sending Failed' });
+          .then(function (history) {
+            next();
           });
       })
-
-  };
+      .catch((err) => {
+        let hash = "none";
+        let desc;
+        if (
+          err.message ===
+          "Returned error: insufficient funds for gas * price + value"
+        ) {
+          res
+            .status(500)
+            .json({ message: "insufficient funds for gas * price + value" });
+          desc = "insufficient funds for gas * price + value";
+        }
+        console.log(err);
+        return accountHistory
+          .create({
+            from: fundAccount,
+            transaction_id: hash,
+            transaction_status: false,
+            value: myValue,
+            to: toAddress,
+            user: user,
+            description: desc,
+            link: `https://etherscan.io/address/${fundAccount}`,
+            ket: text,
+          })
+          .then(function (history) {
+            next(err);
+          })
+          .catch((err) => {
+            res.status(200).json({ message: "Sending Failed" });
+          });
+      });
+  }
 
   static async sendAdminCodeo(req, res, next) {
-    console.log('Masuk sendAdminCodeo ==========================================================')
+    console.log(
+      "Masuk sendAdminCodeo =========================================================="
+    );
     let { adminValue } = req.body;
     let myAccount = req.myAccount;
-    let newKey = JSON.parse(JSON.stringify(myAccount.key))
+    let newKey = JSON.parse(JSON.stringify(myAccount.key));
     let key = await decryptAccount(newKey);
     let adminETH;
-    Account.findOne({ role: 'admin' })
+    Account.findOne({ role: "admin" })
       .then(async function (account) {
         let toAddress = account.ETH;
         adminETH = account.ETH;
-        console.log(adminETH, 'ADMIN ETH -=========================================================')
-        return TransferCodeo(toAddress, adminValue, key)
+        console.log(
+          adminETH,
+          "ADMIN ETH -========================================================="
+        );
+        return TransferCodeo(toAddress, adminValue, key);
       })
       .then(function (trxid) {
         return AdminFeeHistory.create({
@@ -82,66 +99,73 @@ class CodeoTransferController {
           transaction_status: true,
           value: adminValue,
           from: req.decoded.username,
-          admin_address: adminETH
-        })
+          admin_address: adminETH,
+        });
       })
       .then(function (trans) {
-        console.log(trans, 'TRANS ADMIN =========================================')
+        console.log(
+          trans,
+          "TRANS ADMIN ========================================="
+        );
         next();
       })
-      .catch(err => {
-        let hash = 'none';
+      .catch((err) => {
+        let hash = "none";
         AdminFeeHistory.create({
           transaction_id: hash,
           transaction_status: false,
           value: adminValue,
           from: req.decoded.id,
-          admin_address: adminETH
+          admin_address: adminETH,
         })
           .then(function (trans) {
             res.status(200).end();
           })
-          .catch(err => {
+          .catch((err) => {
             res.status(200).end();
-          })
-      })
-  };
+          });
+      });
+  }
 
   static referralStorage(req, res, next) {
-
     let refUser = req.refUser;
 
     if (refUser) {
       User.findOne({ username: refUser.user })
         .then(function (user) {
-          return Referral.create({ user: user.id, ref_amount: refUser.refValue })
+          return Referral.create({
+            user: user.id,
+            ref_amount: refUser.refValue,
+          });
         })
         .then(function (ref) {
           next();
         })
-        .catch(next)
+        .catch(next);
     } else {
       next();
     }
-  };
+  }
 
   static storeHistoryTransaction(req, res, next) {
     let events = req.events;
     TransactionHistory.find({})
       .then(function (trans) {
         if (trans.length == 0) {
-          return TransactionHistory.create({ transactions: events })
+          return TransactionHistory.create({ transactions: events });
         } else {
           let id = trans[0].id;
-          return TransactionHistory.updateOne({ _id: id }, { transactions: events })
+          return TransactionHistory.updateOne(
+            { _id: id },
+            { transactions: events }
+          );
         }
       })
       .then(function (trasactionhistory) {
         res.end();
       })
-      .catch(next)
-  };
-
+      .catch(next);
+  }
 }
 
 module.exports = CodeoTransferController;
--- helpers/codeoTransfer.js
+++ helpers/codeoTransfer.js
@@ -1,56 +1,59 @@
 const Web3 = require("web3");
-var Tx = require('ethereumjs-tx');
-const contractABI = require("../controllers/API/ABI/ABIcodeo")
+var Tx = require("ethereumjs-tx");
+const contractABI = require("../controllers/API/ABI/ABIcodeo");
 
 var web3js = new Web3(new Web3.providers.HttpProvider(process.env.INFURA));
 let { CODEO, ENCRYPT } = process.env;
 
 async function TransferCodeo(toAddress, value, key) {
+  return new Promise((resolve, reject) => {
+    let receipt;
 
-    return new Promise((resolve, reject) => {
+    console.log("Masuk Transfer");
 
-        let receipt;
-
-        console.log('Masuk Transfer')
-
-        let myAddress = key.address;
-        let PriKey = key.privateKey.slice(2);
-        let privateKey = Buffer.from(PriKey, "hex");
-        //contract abi is the array that you can get from the ethereum wallet or etherscan
-        let contractAddress = CODEO;
-        //creating contract object
-        let mytt = new web3js.eth.Contract(contractABI, contractAddress);
-        web3js.eth.getTransactionCount(myAddress).then(function (v) {
-            console.log(v)
-            count = v;
-            let howMuch = value.toString();
-            let amount = web3js.utils.toHex(web3js.utils.toWei(howMuch))
-            let rawTransaction = {
-                from: myAddress,
-                gasPrice: web3js.utils.toHex(45 * 1e9),
-                gasLimit: web3js.utils.toHex(60000),
-                to: contractAddress,
-                value: 0,
-                data: mytt.methods.transfer(toAddress, amount).encodeABI(),
-                nonce: web3js.utils.toHex(count)
-            };
-            let transaction = new Tx(rawTransaction);
-            transaction.sign(privateKey);
-            web3js.eth
-                .sendSignedTransaction("0x" + transaction.serialize().toString("hex"), (err, txHash) => {
-                    console.log('txHash:', txHash)
-                    // Now go check etherscan to see the transaction!
-                    if (err) {
-                        reject({ errors: err.message });
-                    } else {
-                        resolve(txHash);
-                    }
-                })
-        })
-            .catch(err => {
-                reject(err)
-            })
-    })
+    let myAddress = key.address;
+    let PriKey = key.privateKey.slice(2);
+    let privateKey = Buffer.from(PriKey, "hex");
+    //contract abi is the array that you can get from the ethereum wallet or etherscan
+    let contractAddress = CODEO;
+    //creating contract object
+    let mytt = new web3js.eth.Contract(contractABI, contractAddress);
+    web3js.eth
+      .getTransactionCount(myAddress)
+      .then(function (v) {
+        console.log(v);
+        count = v;
+        let howMuch = value.toString();
+        let amount = web3js.utils.toHex(web3js.utils.toWei(howMuch));
+        let rawTransaction = {
+          from: myAddress,
+          gasPrice: web3js.utils.toHex(45 * 1e9),
+          gasLimit: web3js.utils.toHex(60000),
+          to: contractAddress,
+          value: 0,
+          data: mytt.methods.transfer(toAddress, amount).encodeABI(),
+          nonce: web3js.utils.toHex(count),
+        };
+        let transaction = new Tx(rawTransaction);
+        transaction.sign(privateKey);
+        web3js.eth.sendSignedTransaction(
+          "0x" + transaction.serialize().toString("hex"),
+          (err, txHash) => {
+            console.log("txHash:", txHash);
+            // Now go check etherscan to see the transaction!
+            if (err) {
+              //   console.log("Error masuk!");
+              reject(err);
+            } else {
+              resolve(txHash);
+            }
+          }
+        );
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 module.exports = TransferCodeo;
